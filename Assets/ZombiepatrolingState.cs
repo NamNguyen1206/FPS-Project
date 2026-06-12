c6c6150932bd49bc1429608fbc9214eeebf53626
NamNguyen <nguyenmai7922@gmail.com>
2026-06-12 20:38:08 +0700

    push

--- Assets/ZombiepatrolingState.cs
+++ Assets/ZombiepatrolingState.cs
@@ -6,7 +6,7 @@ using System.Collections;
 public class ZombiepatrolingState : StateMachineBehaviour
 {
     float timer;
-    public float patrolingTime = 0f;
+    public float patrolingTime = 10f;
     Transform player;
     NavMeshAgent navAgent;
 
@@ -18,27 +18,33 @@ public class ZombiepatrolingState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       player = GameObject.FindGameObjectWithTag("Player").transform;
-       navAgent = animator.GetComponent<NavMeshAgent>();
+        Debug.Log("ENTER PATROL STATE");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        navAgent = animator.GetComponent<NavMeshAgent>();
 
-       navAgent.speed = patrolspeed;
-       timer = 0;
+        navAgent.speed = patrolspeed;
+        timer = 0;
 
        // -- Get all waypoints and move to first waypoint --//
        waypointList.Clear(); // Clear previous waypoints to avoid duplicates
        GameObject waypointCluster = GameObject.FindGameObjectWithTag("Waypoints");
+       //Debug.Log("Waypoint Cluster = " + waypointCluster);
        if(waypointCluster != null)
        {
-           foreach(Transform waypoint in waypointCluster.transform)
-           {
+            Debug.Log("Child Count = " + waypointCluster.transform.childCount);
+
+            foreach(Transform waypoint in waypointCluster.transform)
+            {
                waypointList.Add(waypoint);
-           }
+               Debug.Log("Waypoint count = " + waypointList.Count);
+            }
        }
        
        if(waypointList.Count > 0)
        {
            Vector3 nextPosition = waypointList[Random.Range(0, waypointList.Count)].position;
            navAgent.SetDestination(nextPosition);
+           Debug.Log("Going to: " + nextPosition);
        }
     }
 
@@ -46,7 +52,7 @@ public class ZombiepatrolingState : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        // Check if we reached the destination and move to next waypoint
-        if(waypointList.Count > 0 && navAgent.remainingDistance < navAgent.stoppingDistance)
+        if(waypointList.Count > 0 && navAgent.remainingDistance <= navAgent.stoppingDistance)
         {
             navAgent.SetDestination(waypointList[Random.Range(0, waypointList.Count)].position);
         }
