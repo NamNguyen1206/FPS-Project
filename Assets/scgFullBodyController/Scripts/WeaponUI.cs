6a28cdc73a8c27b87bc9e887a9e266bf1603070f
NamNguyen <nguyenmai7922@gmail.com>
2026-07-01 21:26:38 +0700

    push

---
+++ Assets/scgFullBodyController/Scripts/WeaponUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WeaponUI : MonoBehaviour
+{
+    [Header("Armor")]
+    public TextMeshProUGUI magazineAmmoUI;
+    public TextMeshProUGUI totalAmmoUI;
+    [Header("Weapon")]
+    public Image activeWeaponUI;
+    [Header("Throwables")]
+    public Image lethalUI;
+    public TextMeshProUGUI lethalAountUI;
+
+    private void Update()
+    {
+        Weapon activeWeapon = WeaponManager.Instance.activeWeaponSlot.GetComponent<Weapon>();
+
+        if(activeWeapon )
+        {
+            magazineAmmoUI.text = $"{activeWeapon.bulletsLeft / activeWeapon.bulletsPerBust}";
+            totalAmmoUI.text = $"{activeWeapon.magazineSize / activeWeapon.bulletsPerBust}";
+            Weapon.WeaponType weaponType = activeWeapon.weaponType;
+
+            activeWeaponUI.sprite = GetWeaponSprite(weaponType);
+        }
+        else
+        {
+            magazineAmmoUI.text = "";
+            totalAmmoUI.text = "";
+        }
+    }
+    private Sprite GetWeaponSprite(Weapon.WeaponType weaponType)
+    {
+        switch (weaponType)
+        {
+            case Weapon.WeaponType.Pistol:
+                return Instantiate(Resources.Load<GameObject>("Pistol")).GetComponent<SpriteRenderer>().sprite;
+            case Weapon.WeaponType.Rifle:
+                return Instantiate(Resources.Load<GameObject>("Rifle")).GetComponent<SpriteRenderer>().sprite;
+            case Weapon.WeaponType.Sniper:
+                return Instantiate(Resources.Load<GameObject>("Sniper")).GetComponent<SpriteRenderer>().sprite;
+            default:
+                return null;
+        }
+    }
+
+}
+
+
+
